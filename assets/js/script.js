b4bdd849b86ed8888f86b12601294823de3183d2
BIBI78 <blakecollins78@gmail.com>
2022-09-04 14:58:47 +0000

    add js random pictures kinda

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,5 @@ const thirdNumber = Math.floor(Math.random()*6) + 1
 console.log(firstNumber);
 console.log(secondNumber);
 console.log(thirdNumber);
+
+const firstDie =  "assets/images/d" + firstNumber + "png";
