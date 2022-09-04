446c09970c94289614f5116ddaf30e15d9dc0ae7
BIBI78 <blakecollins78@gmail.com>
2022-09-04 14:32:14 +0000

    add random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 console.log("slatt")
+
+const firstNumber = Math.floor(Math.random()*6) + 1
+const secondNumber = Math.floor(Math.random()*6) + 1
+const thirdNumber = Math.floor(Math.random()*6) + 1
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(thirdNumber);
