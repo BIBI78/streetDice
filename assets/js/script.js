a7c9b5c02af631c0e3494945daa81b51f450d49d
BIBI78 <blakecollins78@gmail.com>
2022-09-04 18:54:36 +0000

    add attempt at js roll function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,67 @@
 console.log("slatt")
 
-const firstNumber = Math.floor(Math.random()*6) + 1
-const secondNumber = Math.floor(Math.random()*6) + 1
-const thirdNumber = Math.floor(Math.random()*6) + 1
-console.log(firstNumber);
-console.log(secondNumber);
-console.log(thirdNumber);
 
-const firstDie =  "assets/images/d" + firstNumber + ".png";
-const secondDie =  "assets/images/d" + secondNumber + ".png";
-const thirdDie =  "assets/images/d" + thirdNumber + ".png";
 
-document.querySelectorAll('img')[0].setAttribute("src",firstDie);
-document.querySelectorAll('img')[1].setAttribute("src",secondDie);
-document.querySelectorAll('img')[2].setAttribute("src",thirdDie);
+// const firstNumber = Math.floor(Math.random()*6) + 1
+// const secondNumber = Math.floor(Math.random()*6) + 1
+// const thirdNumber = Math.floor(Math.random()*6) + 1
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(thirdNumber);
+
+// const firstDie =  "assets/images/d" + firstNumber + ".png";
+// const secondDie =  "assets/images/d" + secondNumber + ".png";
+// const thirdDie =  "assets/images/d" + thirdNumber + ".png";
+
+// document.querySelectorAll('img')[0].setAttribute("src",firstDie);
+// document.querySelectorAll('img')[1].setAttribute("src",secondDie);
+// document.querySelectorAll('img')[2].setAttribute("src",thirdDie);
+
+// 
+
+const fourthNumber = Math.floor(Math.random()*6) + 1
+const fifthNumber = Math.floor(Math.random()*6) + 1
+const sixthNumber = Math.floor(Math.random()*6) + 1
+console.log(fourthNumber);
+console.log(fifthNumber);
+console.log(sixthNumber);
+
+const fouthDie =  "assets/images/d" + fourthNumber + ".png";
+const fifthDie =  "assets/images/d" + fifthNumber + ".png";
+const sixthDie =  "assets/images/d" + sixthNumber + ".png";
+
+document.querySelectorAll('img')[3].setAttribute("src",fouthDie);
+document.querySelectorAll('img')[4].setAttribute("src",fifthDie);
+document.querySelectorAll('img')[5].setAttribute("src",sixthDie);
+
+// 
+function rollDie(){
+    document.getElementsByClassName("dieImage")[0]
+    document.getElementsByClassName("dieImage")[1]
+    document.getElementsByClassName("dieImage")[2]
+    document.getElementsByClassName("dieImage")[3]
+    document.getElementsByClassName("dieImage")[4]
+    document.getElementsByClassName("dieImage")[5]
+
+
+
+
+
+// const firstNumber = Math.floor(Math.random()*6) + 1
+// const secondNumber = Math.floor(Math.random()*6) + 1
+// const thirdNumber = Math.floor(Math.random()*6) + 1
+
+// const firstDie =  "assets/images/d" + firstNumber + ".png";
+// const secondDie =  "assets/images/d" + secondNumber + ".png";
+// const thirdDie =  "assets/images/d" + thirdNumber + ".png";
+
+// document.querySelectorAll('img')[0].setAttribute("src",firstDie);
+// document.querySelectorAll('img')[1].setAttribute("src",secondDie);
+// document.querySelectorAll('img')[2].setAttribute("src",thirdDie);
+
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(thirdNumber);
+
+}
+
