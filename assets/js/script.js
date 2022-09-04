dfff99c62acd7abee36ed8bdd4f5f6e6dca74daf
BIBI78 <blakecollins78@gmail.com>
2022-09-04 16:16:22 +0000

    add die images play with random number

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,10 @@ console.log(firstNumber);
 console.log(secondNumber);
 console.log(thirdNumber);
 
-const firstDie =  "assets/images/d" + firstNumber + "png";
+const firstDie =  "assets/images/d" + firstNumber + ".png";
+const secondDie =  "assets/images/d" + secondNumber + ".png";
+const thirdDie =  "assets/images/d" + thirdNumber + ".png";
+
+document.querySelectorAll('img')[0].setAttribute("src",firstDie);
+document.querySelectorAll('img')[1].setAttribute("src",secondDie);
+document.querySelectorAll('img')[2].setAttribute("src",thirdDie);
