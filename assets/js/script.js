181e354b9c09e95793e4c5da0af96f6c1cdd1943
BIBI78 <blakecollins78@gmail.com>
2022-09-05 12:11:59 +0000

    Add basic structure of the roll functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,21 +71,80 @@ function rollDie(){
     const secondNumber = Math.floor(Math.random()*6) + 1;
     const thirdNumber = Math.floor(Math.random()*6) + 1;
 
+
+
     const firstDie =  "assets/images/d" + firstNumber + ".png";
     const secondDie =  "assets/images/d" + secondNumber + ".png";
     const thirdDie =  "assets/images/d" + thirdNumber + ".png";
-    // document.getElementById('img')[0].setAttribute("src",firstDie); 
+
+    document.getElementsByTagName('img')[0].setAttribute("src",firstDie); 
     document.getElementsByTagName('img')[1].setAttribute("src",secondDie); 
     document.getElementsByTagName('img')[2].setAttribute("src",thirdDie); 
 
-    document.querySelector('img').setAttribute("src",firstDie); 
-    // document.querySelector('img').setAttribute("src",secondDie); 
-    // document.querySelector('img').setAttribute("src",thirdDie); 
-    
-
-
-
+    console.log(firstNumber);
+    console.log(secondNumber);
+    console.log(thirdNumber);
+    console.log("slatt");
     
   }
 
+
+  function rollDieTwo() {
+
+    const fourthNumber = Math.floor(Math.random()*6) + 1;
+    const fifthNumber = Math.floor(Math.random()*6) + 1;
+    const sixthNumber = Math.floor(Math.random()*6) + 1;
+
+
+
+    const fourthDie =  "assets/images/d" + fourthNumber + ".png";
+    const fifthDie =  "assets/images/d" + fifthNumber + ".png";
+    const sixthDie =  "assets/images/d" + sixthNumber + ".png";
+
+    document.getElementsByTagName('img')[3].setAttribute("src",fourthDie); 
+    document.getElementsByTagName('img')[4].setAttribute("src",fifthDie); 
+    document.getElementsByTagName('img')[5].setAttribute("src",sixthDie); 
+
+    console.log(fourthNumber);
+    console.log(fifthNumber);
+    console.log(sixthNumber);
+    console.log("blatt");
+
+
+  }
+
+  function rollBoth(){
+    const firstNumber = Math.floor(Math.random()*6) + 1;
+    const secondNumber = Math.floor(Math.random()*6) + 1;
+    const thirdNumber = Math.floor(Math.random()*6) + 1;
+    const fourthNumber = Math.floor(Math.random()*6) + 1;
+    const fifthNumber = Math.floor(Math.random()*6) + 1;
+    const sixthNumber = Math.floor(Math.random()*6) + 1;
+
+
+    const firstDie =  "assets/images/d" + firstNumber + ".png";
+    const secondDie =  "assets/images/d" + secondNumber + ".png";
+    const thirdDie =  "assets/images/d" + thirdNumber + ".png";
+    const fourthDie =  "assets/images/d" + fourthNumber + ".png";
+    const fifthDie =  "assets/images/d" + fifthNumber + ".png";
+    const sixthDie =  "assets/images/d" + sixthNumber + ".png";
+
+    document.getElementsByTagName('img')[0].setAttribute("src",firstDie); 
+    document.getElementsByTagName('img')[1].setAttribute("src",secondDie); 
+    document.getElementsByTagName('img')[2].setAttribute("src",thirdDie); 
+    document.getElementsByTagName('img')[3].setAttribute("src",fourthDie); 
+    document.getElementsByTagName('img')[4].setAttribute("src",fifthDie); 
+    document.getElementsByTagName('img')[5].setAttribute("src",sixthDie); 
+
+    console.log(firstNumber);
+    console.log(secondNumber);
+    console.log(thirdNumber);
+    console.log(fourthNumber);
+    console.log(fifthNumber);
+    console.log(sixthNumber);
+    console.log("free YSL");
+
+
+
+  }
 
