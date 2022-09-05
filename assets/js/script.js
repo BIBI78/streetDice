c16ee90e54b8d6613de51de5c1693e3cf44d4297
BIBI78 <blakecollins78@gmail.com>
2022-09-05 10:24:10 +0000

    add attempt to fix roll function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,32 +19,32 @@ console.log("slatt")
 
 // 
 
-const fourthNumber = Math.floor(Math.random()*6) + 1
-const fifthNumber = Math.floor(Math.random()*6) + 1
-const sixthNumber = Math.floor(Math.random()*6) + 1
-console.log(fourthNumber);
-console.log(fifthNumber);
-console.log(sixthNumber);
+// const fourthNumber = Math.floor(Math.random()*6) + 1
+// const fifthNumber = Math.floor(Math.random()*6) + 1
+// const sixthNumber = Math.floor(Math.random()*6) + 1
+// console.log(fourthNumber);
+// console.log(fifthNumber);
+// console.log(sixthNumber);
 
-const fouthDie =  "assets/images/d" + fourthNumber + ".png";
-const fifthDie =  "assets/images/d" + fifthNumber + ".png";
-const sixthDie =  "assets/images/d" + sixthNumber + ".png";
+// const fouthDie =  "assets/images/d" + fourthNumber + ".png";
+// const fifthDie =  "assets/images/d" + fifthNumber + ".png";
+// const sixthDie =  "assets/images/d" + sixthNumber + ".png";
 
-document.querySelectorAll('img')[3].setAttribute("src",fouthDie);
-document.querySelectorAll('img')[4].setAttribute("src",fifthDie);
-document.querySelectorAll('img')[5].setAttribute("src",sixthDie);
+// document.querySelectorAll('img')[3].setAttribute("src",fouthDie);
+// document.querySelectorAll('img')[4].setAttribute("src",fifthDie);
+// document.querySelectorAll('img')[5].setAttribute("src",sixthDie);
 
 // 
-function rollDie(){
-    document.getElementsByClassName("dieImage")[0]
-    document.getElementsByClassName("dieImage")[1]
-    document.getElementsByClassName("dieImage")[2]
-    document.getElementsByClassName("dieImage")[3]
-    document.getElementsByClassName("dieImage")[4]
-    document.getElementsByClassName("dieImage")[5]
+// function rollDie(){
+//     document.getElementsByClassName("dieImage")[0] 
+//     document.getElementsByClassName("dieImage")[1]
+//     document.getElementsByClassName("dieImage")[2]
+//     document.getElementsByClassName("dieImage")[3]
+//     document.getElementsByClassName("dieImage")[4]
+//     document.getElementsByClassName("dieImage")[5]
 
 
-
+    
 
 
 // const firstNumber = Math.floor(Math.random()*6) + 1
@@ -63,5 +63,5 @@ function rollDie(){
 // console.log(secondNumber);
 // console.log(thirdNumber);
 
-}
+// }
 
