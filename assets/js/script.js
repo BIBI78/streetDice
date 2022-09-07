83a1c3bcb90c13d885950e3331fda922db0f71c7
BIBI78 <blakecollins78@gmail.com>
2022-09-07 17:07:23 +0000

    Add tinker with spin function not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,33 @@
 //   }
 
 
-  function spin(){
-    document.getElementById('img').className = 'spinImg';
+  // function spin(){
+  //    document.getElementById('img').className = 'spinImg';
+   
+  // }
 
+  // function spin(){
+  //   document.getElementById('img').className = 'spinImg'
+  //    }
+   
+  // document.getElementById("spin").addEventListener("click", spin);
+
+ 
+
+  document.getElementById("myBtn").addEventListener("click", displayDate);
+
+  function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
   }
+
+
+
+  document.getElementById("mySpin").addEventListener("click", spin);
+
+  function spin(){
+   document.getElementById('img').className = 'spinImg';
+   
+   
+  
+ }
+
